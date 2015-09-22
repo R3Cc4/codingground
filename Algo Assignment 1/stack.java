1eb3f9f01ec0b99864e9b1cb72f3e91eb56dffe7
vamsi krishna patchamatla <R3Cc4@users.noreply.github.com>
2015-09-22 18:09:33 -0400

    Created by Coding Ground Client

---
+++ Algo Assignment 1/stack.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class stack{
+    private ArrayList stack = new ArrayList();
+    private  int top=0;
+    public void push(String value){
+        stack.add(top,value);
+        System.out.println("Pushed value =="+value);
+        top++;
+        
+    }
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+    public String pop(){
+    String value;    
+        if(!stack.isEmpty()){
+            top--;
+            value=(String)stack.get(top);
+            System.out.println("Popped Value =="+(String)stack.get(top));
+            stack.remove(top);
+            return value;
+            
+        }else{
+        System.out.println("Stack Is Empty!!");
+        return "Stack Is Empty!!";
+        }
+    }
+}
