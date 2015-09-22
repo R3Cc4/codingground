fd8c39c180e475e12f8f6655a29639bd262e21f8
vamsi krishna patchamatla <R3Cc4@users.noreply.github.com>
2015-09-22 18:09:55 -0400

    Created by Coding Ground Client

---
+++ Algo Assignment 1/FindExpression.java
@@ -0,0 +1,17 @@
+
+public class FindExpression {
+
+     public static void main(String []args){
+       stack stack1 = new stack();
+       stack1.push("34");
+       stack1.push("54");
+       stack1.push("12");
+       stack1.pop();
+       stack1.pop();
+       stack1.push("24");
+       stack1.pop();
+       stack1.pop();
+       stack1.pop();
+       
+     }
+}
